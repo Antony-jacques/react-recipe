ef78a5a93d8240fe8ec101a5885f4f8344a60263
Antony <antony.jacques@laposte.net>
2021-07-27 09:46:47 +0200

    init display

---
+++ src/Components/Content/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Content.css'
+
+const Content = () => {
+    return (
+        <div className="content">
+            <h1>Bienvenue sur notre site de recettes</h1>
+        </div>
+    );
+}
+
+export default Content;
---
+++ src/Components/CountriesSelector/CountriesSelector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./CountriesSelector.css"
+
+const CountriesSelector = () => {
+    return (
+        <div className="CountriesSelector">
+            countries
+        </div>
+    );
+}
+
+export default CountriesSelector;
