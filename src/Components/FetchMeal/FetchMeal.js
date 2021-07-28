aadfcc34acf50a97d6bc03463c43b3c080afd8e2
Antony <antony.jacques@laposte.net>
2021-07-28 11:01:40 +0200

    fetch data & setState

--- src/Components/FetchMeal/FetchMeal.js
+++ src/Components/FetchMeal/FetchMeal.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useContext, useState, useEffect} from 'react';
+import {MyContext} from '../Context/Context.js'
+
 
 const FetchMeal = () => {
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
-    .then(response => response.json())
-    .then(response => console.log(response))
+    
+    const {lang} = useContext(MyContext)
+    
+     const [ recipeArray, setRecipeArray] = useState([]);
+    
+    const getRecipe = ()=>{
 
+            fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
+            .then(response => response.json()) // transforme la rp en json afin de pouvoir la parcourir
+            .then(response => {
+                setRecipeArray(response.meals)
+            })
+        
+    }
+    
+    
+   useEffect(getRecipe,[])
+    
+    
+
+    console.log('recipeArray', recipeArray)
 
     return (
         <div>
