11a614e3f075e0c997056677815ab7f3a428e80f
Antony <antony.jacques@laposte.net>
2021-08-05 08:48:20 +0200

    fix deploy bug

--- src/Components/RandomRecipe/RandomRecipe.js
+++ src/Components/RandomRecipe/RandomRecipe.js
@@ -11,21 +11,20 @@ import Row from "react-bootstrap/Row";
 const RandomRecipe = () => {
 
   
-  const [randomMeal, setRandomMeal] = useState({});
+  const [randomMeal, setRandomMeal] = useState([]);
 
   const { lang } = useContext(MyContext);
 
   const fetchRandomRecipe = () => {
-    fetch("https:www.themealdb.com/api/json/v1/1/random.php")
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+      .then((response) =>  response.json())
       .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-         setRandomMeal(response.meals[0]);
+         setRandomMeal(response.meals);
       });
   };
 
-  // console.log("randomMeal", randomMeal);
+    console.log("randomMeal", randomMeal);
+  //  console.log("randomMeal[0].strMealThumb", randomMeal[0].strMealThumb);
 
   useEffect(fetchRandomRecipe, []);
 
@@ -36,27 +35,35 @@ const RandomRecipe = () => {
       >
         <Row>
           <h2>{traductions[lang].subtitle}</h2>
+          <h2>test</h2>
 
-          {/* {randomMeal && ( */}
+          {randomMeal[0] && (
+
+
             <Link
               to={{
                 pathname: "/single",
                 state: {
-                  idMeal: randomMeal.idMeal,
+                  idMeal: randomMeal[0].idMeal,
                 },
               }}
               className="recipe-card"
             >
               <Card>
-                <Card.Img variant="top" src={randomMeal.strMealThumb} />
+                <Card.Img variant="top" src={randomMeal[0].strMealThumb} />
                 <Card.Body>
-                  <Card.Title>{randomMeal.strMeal}</Card.Title>
+                  <Card.Title>
+                    {randomMeal[0].strMeal}
+                    <h2>test</h2>
+                    </Card.Title>
                   <Card.Text></Card.Text>
-                  {/* <Button variant="primary">Go somewhere</Button> */}
                 </Card.Body>
               </Card>
             </Link>
-           {/* )} */}
+
+
+            
+            )} 
         </Row>
       </Container>
     </div>
